025b826a63f4de44086f543e22596f24f005901c
Alpa Mehta <alpapmehta27@gmail.com>
2026-05-14 19:16:45 -0500

    Python Lab1 Part 1,2 & 3

---
+++ hello.py
@@ -0,0 +1,37 @@
+print ("Part 1")
+print()
+print ("""1.	What is computer programming, and why is it important in today’s /n
+       technology-driven world?
+Computer programming is the process of designing and building executable, step-by-step
+instructions code that enables computers to solve problems, automate tasks, and 
+perform specific functions. It is important today because it powers almost all
+modern technology, from smartphones and websites to medical equipment and global
+infrastructure, driving efficiency and innovation. """)
+print()
+print ("""2   Briefly describe what Python is and one reason it is a popular 
+programming language for beginners. Python is a high-level, general-purpose 
+programming language designed with an emphasis on code readability. It is 
+an interpreted language, meaning the code is executed line-by-line, which helps
+developers quickly identify and fix errors. 
+A primary reason for its popularity among beginners is its simple, English-like
+ syntax. Unlike many other languages that use complex symbols or rigid structural 
+requirements.""")
+print()
+print ("""3	What do you need (software/tools) to start writing and running Python
+ programs on your own computer?
+To start writing and running Python programs on your own computer in 2026, you 
+need two essential components: the Python Interpreter (to run code) and a Code 
+Editor or IDE  to write code.""")
+print()
+print ("Part 2")
+print()
+print ("Hello, World!")
+print ("Welcome to Python programming.")
+print()
+print ("Part 3") 
+print  ("Hello, World!")
+print ("Welcome to Python programming.")
+print()
+name = input("What is your name? ")
+print()
+print("Hello, " + name + "!" + "Glad to have you learning Python.")
